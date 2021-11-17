3f477a6fd8976f36251cb6e015741049e1ff967e
Yassine Zemmouri <yassine81ma@gmail.com>
2021-11-17 15:37:46 -0500

    completed tasks in conditionals and loops

---
+++ conditionals.js
@@ -0,0 +1,23 @@
+// =====Conditionals
+// Definition: perform differnet operations based on the given conditon
+// let integer = -1;
+
+// if (integer < 0) {
+//   console.log("Negative: " + integer);
+// } else {
+//   console.log("Psitive: " + integer);
+// }
+// for (let i = 1; i < 10; i += 2) console.log(i);
+// for (let i = 2; i < 11; i += 2) console.log(i);
+// for (var i = 0; i < 5; ++i) {
+//   console.log(i);
+// }
+// let i = 1;
+// let z = 0;
+// let j = 1;
+// z = ++i;
+// f = j++;
+// console.log(z);
+// console.log(f);
+
+g;
